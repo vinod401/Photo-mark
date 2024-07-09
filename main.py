0c27c91d161904f9708085cfac7042af2a8ff12e
Vinod <vinodvino113@gmail.com>
2024-07-09 13:36:29 +0530

    simple gui to add image , change text and font of the watermakr, and display result image

--- main.py
+++ main.py
@@ -1,23 +1,121 @@
-from tkinter.filedialog import askopenfilename
-from PIL import Image
+from tkinter import *
+from tkinter import ttk, font
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from PIL import Image, ImageTk
 from text_watermark import TextMark
 
-
+BASE_IMAGE = None
+DISPLAY_IMAGE = None
+MAX_SIZE = (720, 560)
 textmark = TextMark()
 
-try:
+
+def update_textbox(event):
+    textmark.text = text_box.get()
+    textmark_active()
+
+
+def textmark_active():
+    global DISPLAY_IMAGE, BASE_IMAGE
+    text_box.config(state="normal")
+    fonts.config(state="readonly")
+
+    textmark.image_to_make(image=BASE_IMAGE)
+    textmark.default_pos()
+    DISPLAY_IMAGE = textmark.result_image
+    DISPLAY_IMAGE.thumbnail(MAX_SIZE)
+    DISPLAY_IMAGE = ImageTk.PhotoImage(DISPLAY_IMAGE)
+
+    canvas.config(width=720, height=560)
+    canvas.create_image(360, 280, image=DISPLAY_IMAGE)
+    canvas.image = DISPLAY_IMAGE
+
+
+def font_changed(event):
+    textmark.font = current_font.get()
+    textmark_active()
+
+
+def image_mark_active():
+    fonts.config(state="disabled")
+    text_box.config(state="disabled")
+
+
+def upload_image():
+    global BASE_IMAGE
+    # saving the file path of the image to be watermarked only jpeg or png can be selected
+
     img_path = askopenfilename(initialdir="../Users/<name>/Pictures",
-                                   title="Select A File", filetype=(("jpg", "*.jpg"), ("png", "*.png"))).strip()
+                               title="Select A File", filetype=(("jpg", "*.jpg"), ("png", "*.png"))).strip()
+    if img_path:
+        BASE_IMAGE = Image.open(img_path)
 
-except PermissionError:
-    pass
+        text_radio_btn.config(state="normal")
+        image_radio_btn.config(state="normal")
+        textmark_active()
 
-else:
-    image = Image.open(img_path)
-    textmark.image_to_make(image=image)
 
-    x = image.width - 200
-    y = image.height - 75
+def create_text_mark():
+    # saving the file path of the image to be watermarked only jpeg or png can be selected
 
-    textmark.update_pos(x=x, y=y)
-    textmark.make_watermark()
+    img_path = askopenfilename(initialdir="../Users/<name>/Pictures",
+                               title="Select A File", filetype=(("jpg", "*.jpg"), ("png", "*.png"))).strip()
+    if img_path:
+        image = Image.open(img_path)
+
+        textmark.image_to_make(image=image)
+        textmark.default_pos()
+
+        # making text mark
+        # textmark.make_watermark()
+        textmark.result_image.show()
+    else:
+        pass
+
+
+def save_image():
+    # save image as a new file
+    path = asksaveasfilename(confirmoverwrite=True, defaultextension="png",
+                             filetypes=[("png", ".png"), ("jpeg", ".jpg")])
+    if path:
+        textmark.result_image.save(path)
+
+
+root = Tk()
+
+upload_button = ttk.Button(text="Upload", command=upload_image)
+upload_button.pack()
+
+radio_value = StringVar(value="text")
+
+# ----------------------------------------------text watermark -----------------------------------------------------
+text_radio_btn = ttk.Radiobutton(root, text="Text Water Mark", value="text", variable=radio_value, state="disabled",
+                                 command=textmark_active)
+text_radio_btn.pack()
+text_box = ttk.Entry()
+text_box.pack()
+text_box.bind("<KeyRelease>", update_textbox)
+text_box.insert(index=1, string=textmark.text)
+text_box.config(state="disabled")
+
+current_font = StringVar()
+fonts = ttk.Combobox(root, width=10, state="readonly", textvariable=current_font)
+fonts["values"] = textmark.font_list
+fonts.bind("<<ComboboxSelected>>", font_changed)
+fonts.pack()
+fonts.current(2)
+fonts.config(state="disabled")
+# --------------------------------------------------------------------------------------------------------------------
+
+# -------------------------------------image watermark --------------------------------------------------------------
+image_radio_btn = ttk.Radiobutton(root, text="Image Water Mark", value="image", variable=radio_value, state="disabled",
+                                  command=image_mark_active)
+image_radio_btn.pack()
+
+# -------------------------------------------------------------------------------------------------------------------
+
+canvas = Canvas(root, width=0, height=0)
+canvas.pack()
+
+
+root.mainloop()
--- text_watermark.py
+++ text_watermark.py
@@ -1,36 +1,93 @@
 from PIL import ImageFont
 from PIL import ImageDraw, Image
+import matplotlib.font_manager as fm
 
 
 class TextMark:
     def __init__(self):
+        self.font_list = sorted(set([f.name for f in fm.fontManager.ttflist]))
         self.text = "PHOTOMARK"
         self.font = "arial"
+        self.opacity = 100
         self.size = 40
-        self.color = "#ffffff"
+        self.color = (255, 255, 255, self.opacity)
+        self.text_width = None
+        self.text_height = None
         self.x_pos = 0
         self.y_pos = 0
+        self.rotation = 0
+
         self.image = None
+        self.rotated_image = None
         self.result_image = None
 
-    def make_watermark(self):
-        self.result_image = self.image.copy()
-        font = ImageFont.truetype(self.font, self.size)
-        draw = ImageDraw.Draw(self.result_image)
-        draw.text((self.x_pos, self.y_pos), self.text, self.color, font=font, anchor='ms')
-        self.result_image.show()
+    def make_water_mark(self):
+        # font = ImageFont.truetype(self.font, self.size)
+        try:
+            font = ImageFont.truetype(fm.findfont(fm.FontProperties(family=self.font, style=None)), self.size)
+        except ValueError:
+            pass
 
+        else:
+
+            # determining the size of the text in the give font and size
+            self.text_width = font.getbbox(self.text)[2]
+            self.text_height = font.getbbox(self.text)[3]
+
+            # creating a blank transparent image in the size of the text
+            blank_transparent_image = Image.new("RGBA", (self.text_width, self.text_height), (255, 255, 255, 0))
+
+            # drawing the text in the blank image created
+            draw = ImageDraw.Draw(mode="RGBA", im=blank_transparent_image)
+            draw.text((0, 0), text=self.text, fill=self.color, font=font, )
+
+            # applying the rotation if any by default is zero
+            # the expand parameter is to avoid cropping of text while rotating
+            self.rotated_image = blank_transparent_image.rotate(angle=self.rotation, expand=True)
+            self.text_width = self.rotated_image.width
+            self.text_height = self.rotated_image.height
+
+            self.make_final_image()
+
+    def make_final_image(self):
+        """makes the text watermark given in the photo"""
+
+        self.result_image = self.image.copy()
+        self.result_image = self.result_image.convert("RGBA")
+
+        new = Image.new("RGBA", self.result_image.size, (255, 255, 255, 0),)
+        new.paste(self.rotated_image, (self.x_pos, self.y_pos))
+        self.result_image = Image.alpha_composite(self.result_image, new, )
+
+    def default_position(self):
+        pass
 
     def update_pos(self, x, y):
-
+        """receives x and y position to place the text in the image"""
         self.x_pos = x
         self.y_pos = y
+        self.make_water_mark()
 
+    def default_pos(self):
+        self.make_water_mark()
+        width = self.image.width
+        height = self.image.height
+        self.x_pos = width - self.text_width - 20
+        self.y_pos = height - self.text_height - 20
+        self.make_water_mark()
+
+
+    def update_rotation(self, rotation):
+        """receives rotation as parameter and assign to the rotation of the watermark"""
+        self.rotation = rotation
+        self.make_water_mark()
+
+
+    def update_font(self, font):
+        self.font = font
 
 
     def image_to_make(self, image):
+        """receives an image to make the watermark"""
         self.image = image
-
-
-
 
